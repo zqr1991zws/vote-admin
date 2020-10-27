32837fa78320b10269fa5641ebd7e794293dff29
java <136632801@qq.com>
2020-10-27 13:41:26 +0800

    后端用户验证

---
+++ src/main/java/com/zws/vote/VoteApplication.java
@@ -0,0 +1,13 @@
+package com.zws.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zws/vote/config/AuthorizationServerConfig.java
@@ -0,0 +1,4 @@
+package com.zws.vote.config;
+
+public class AuthorizationServerConfig {
+}
